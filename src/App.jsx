120d0790ca1a973d05b5b99636f4dd8f09cd4ab2
kikoolz <kenlubs45@gmail.com>
2025-11-08 16:52:15 +0400

    Enhance application structure by adding new routes for Preview and Login pages, and update Home page with Banner, Hero, and Features components. Introduce Outlet in Layout for nested routing. Add button cursor styling in CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Routes path="app" element={<Layout />}>
+
+        <Route path="app" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="builder/:resumeId" element={<ResumeBuilder />} />
-        </Routes>
+        </Route>
+
+        <Route path="view/:resumeId" element={<Preview />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </>
   );
